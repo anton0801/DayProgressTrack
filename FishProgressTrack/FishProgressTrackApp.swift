75bd16ee1c44116440febd9c9f188cbe86936ed1
Stepan <sfermin2007@icloud.com>
2026-01-18 20:04:03 +0100

    Initial Commit

---
+++ FishProgressTrack/FishProgressTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishProgressTrackApp.swift
+//  FishProgressTrack
+//
+//  Created by Stepan Yarikova on 18/1/26.
+//
+
+import SwiftUI
+
+@main
+struct FishProgressTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FishProgressTrackTests/FishProgressTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  FishProgressTrackTests.swift
+//  FishProgressTrackTests
+//
+//  Created by Stepan Yarikova on 18/1/26.
+//
+
+import Testing
+@testable import FishProgressTrack
+
+struct FishProgressTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
